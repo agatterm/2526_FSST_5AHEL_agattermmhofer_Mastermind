db45afbbfdaa129ea25e46060cc82b1f1ddd0c81
Alex <agatter@htl-steyr.ac.at>
2026-03-19 10:25:20 +0100

    V1

--- Mastermind/src/main/java/org/example/mastermind/HelloApplication.java
+++ Mastermind/src/main/java/org/example/mastermind/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MastermindV1"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 550);
 
         // MastermindView wurde von FXMLLoader als Controller instanziiert
